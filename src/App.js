4d2964f52b82fb8309b528c95676c10f42bf16d7
Pranav Vinodan <118279097+Pranav2456@users.noreply.github.com>
2023-11-03 18:46:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+// Refer to the README doc for more information about using API
+// keys in client-side code. You should never do this in production
+// level code.
+const settings = {
+  apiKey: "THXWPdHKMa8Qsap08ju-HyIwIMm0eBY9",
+  network: Network.ETH_SEPOLIA,
+};
+
+// In this week's lessons we used ethers.js. Here we are using the
+// Alchemy SDK is an umbrella library with several different packages.
+//
+// You can read more about the packages here:
+//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
+const alchemy = new Alchemy(settings);
+
+function App() {
+  const [blockNumber, setBlockNumber] = useState();
+
+  useEffect(() => {
+    async function getBlockNumber() {
+      setBlockNumber(await alchemy.core.getBlockNumber());
+    }
+
+    getBlockNumber();
+  });
+
+  return <div className="App">Block Number: {blockNumber}</div>;
+}
+
+export default App;
