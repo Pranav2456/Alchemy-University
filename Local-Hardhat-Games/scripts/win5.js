2887269e16847c9696dcb130cfa7e746a97b6796
Pranav Vinodan <118279097+Pranav2456@users.noreply.github.com>
2023-11-24 09:13:40 +0530

    Changes done

---
+++ Local-Hardhat-Games/scripts/win5.js
@@ -0,0 +1,24 @@
+const gameAddr = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+const contractName = "Game5";
+
+async function main() {
+  // attach to the game
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+  // do whatever you need to do to win the game here:
+  await game.giveMeAllowance(20000);
+  await game.mint(16000);
+  const tx = await game.win();
+
+  // did you win? Check the transaction receipt!
+  // if you did, it will be in both the logs and events array
+  const receipt = await tx.wait();
+  console.log(receipt);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
